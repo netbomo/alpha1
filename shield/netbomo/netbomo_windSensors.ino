cf729911289997f103ad09249fe7c1be2f8b669d
netbomo <gilou@tripalium.org>
2014-07-20 09:52:43 +0200

    update netbomo_windSensors.ino

--- shield/netbomo/netbomo_windSensors.ino
+++ shield/netbomo/netbomo_windSensors.ino
@@ -4,7 +4,7 @@ void windSpeed() {//interruption pour comptage des impulsions pour l'anémometre
 /* Calculer la direction du vent entre deux fronts montant*/
 
   ibiTime_anemo_1 = millis(); // On ancre le temps
-  float hertz = (float)1/(ibiTime_anemo_1-lastTime_anemo_1);
+  float hertz = (float)1000/(ibiTime_anemo_1-lastTime_anemo_1);
   windPower = (windPower + hertz*0.765+0.35)/2; // calibrage en m/s avec données anémo 
   lastTime_anemo_1 = ibiTime_anemo_1;
 
@@ -12,6 +12,7 @@ void windSpeed() {//interruption pour comptage des impulsions pour l'anémometre
 }
 
 void powerProduct(){
+  ibiTime_PowerProduct=millis();
   powerProductMoy= (powerProductMoy + 0.750*1000/(ibiTime_PowerProduct-lastTime_PowerProduct)*3600)/2;
   lastTime_PowerProduct = ibiTime_PowerProduct;
 }
