6a39742e7ca563e488fcb814d9e05db0551a95b9
netbomo <gilou@tripalium.org>
2014-08-02 18:50:13 +0200

    update netbomo.ino

--- shield/netbomo/netbomo.ino
+++ shield/netbomo/netbomo.ino
@@ -1,13 +1,12 @@
 
-// Définitions pour l'anémomètre
-const int Anemo_1 = 3; 
+// Définitions pour l'anémomètre 
 float windPower = 0; 
+float hertz =0;
 int ibiTime_anemo_1 = 0;
 int lastTime_anemo_1 = 0;
 int windVaneOffSet = 140;
 
 // Définitions pour le wattNode
-const int WattNode = 2;
 float powerProductMoy = 0;
 int ibiTime_PowerProduct=0;
 int lastTime_PowerProduct = 0; 
@@ -21,37 +20,44 @@ int dirVentMoy=0;
 int node_id=6;  // Référence du node pour Emoncms
 
 // déclaration temps de moyennage : ici 10 min
-int echantillonageToSendData = 600000;
-int echantillonageWindVane = 1000;
-int global_lastTime;
+unsigned long echantillonageToSendData = 600000;
+unsigned long echantillonageWindVane = 5000;
+unsigned long global_lastTime;
+unsigned long time_test=0;
 
 void setup() { 
 
   Serial.begin(9600); // Initialisation de la liaison serie
 
   attachInterrupt(1, windSpeed, RISING); 
+  attachInterrupt(0, powerProduct, RISING);
 
   lastTime_anemo_1 = millis(); 
   lastTime_PowerProduct = millis();
-  global_lastTime = millis();
+  global_lastTime = 0;
 
 }
 
 
 void loop(){
-  
-  if (global_lastTime-millis()>=echantillonageWindVane) windVaneDir();
 
-  if (global_lastTime-millis()>=echantillonageToSendData){
-  
+  time_test = millis()-global_lastTime;
+
+  if (time_test/echantillonageWindVane>=1) {
+    windVaneDir();
+  }
+
+  if (time_test/echantillonageToSendData>=1){
+
     noInterrupts();
-    
+
     sendData();
-    
+
     interrupts();
-    
+    global_lastTime=millis(); 
   }
 } 
+
 
 
 
--- shield/netbomo/netbomo_windSensors.ino
+++ shield/netbomo/netbomo_windSensors.ino
@@ -4,7 +4,7 @@ void windSpeed() {//interruption pour comptage des impulsions pour l'anémometre
 /* Calculer la direction du vent entre deux fronts montant*/
 
   ibiTime_anemo_1 = millis(); // On ancre le temps
-  float hertz = (float)1/(ibiTime_anemo_1-lastTime_anemo_1);
+  hertz = (float)1000/(ibiTime_anemo_1-lastTime_anemo_1);
   windPower = (windPower + hertz*0.765+0.35)/2; // calibrage en m/s avec données anémo 
   lastTime_anemo_1 = ibiTime_anemo_1;
 
